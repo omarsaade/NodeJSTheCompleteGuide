1e0c7ce824d6f94fcd8bf97da70cabc3a794ec66
omar saadeh <omarsaadeh11@gmail.com>
2022-11-22 12:55:08 -0800

    Updated a Published Package

---
+++ omars/app.js
@@ -0,0 +1,5 @@
+var lion = require("omarsaadepack");
+// var result = lion.add(1, 2);
+// console.log(result);
+var xz = lion.multiply(3, 4);
+console.log(xz);
---
+++ omars/node_modules/omarsaadepack/index.js
@@ -0,0 +1,8 @@
+//entry point to our package
+module.exports.add = (a, b) => {
+  return a + b;
+};
+
+module.exports.multiply = (a, b) => {
+  return a * b;
+};
