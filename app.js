bb6025107ba6467b29f24dccae833a0435e84269
omar saadeh <omarsaadeh11@gmail.com>
2022-11-22 07:41:19 -0800

    nodejs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
-//for this we can create a web server
-//this server is an event emitter
+//we can create a web server
+//this server is an event emitter // create server
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.write("Hello world");
@@ -17,7 +17,14 @@ const server = http.createServer((req, res) => {
 //   console.log("New Connection...");
 // });
 
-//listening
+/*
+raising event
+This function is asynchronous. When the server starts listening,
+ the 'listening' event will be emitted. The last parameter 
+ callbackwill
+ be added as a listener for the 'listening' event.
+ */
+
 server.listen(3000);
 
 console.log("Listening on port 3000...");
